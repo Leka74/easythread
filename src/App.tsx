fa7f4c859ec0cfcbceda0d7deda5975788169ed2
Leke <leke.dobruna@gmail.com>
2024-09-17 00:01:09 +0200

    update example App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import styles from "./App.module.css";
 
+/** @easythread */
+function heavyTask(count: number = 1): Promise<string> {
+  // emulate a heavy computation
+  for (let i = 0; i < count; i++) {
+    // no-op
+  }
+  return Promise.resolve(new Date().toISOString());
+}
+
 function App() {
+  const [state, setState] = useState<string>("");
   return (
     <div className={styles.container}>
       <h1>EasyThread</h1>
+      <div>{state ? state : "No state"}</div>
+      <button onClick={() => heavyTask(5000000000).then(setState)}>
+        Generate heavy task
+      </button>
     </div>
   );
 }
