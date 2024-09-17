e13e05946fe9f42f9131ee20acc991b918b23d4a
Leke <leke.dobruna@gmail.com>
2024-09-17 23:27:36 +0200

    transform `.js` and `.jsx` files

--- easythread-plugin.ts
+++ easythread-plugin.ts
@@ -6,7 +6,12 @@ export default function easythreadPlugin(): Plugin {
     name: "vite-plugin-easythread",
     enforce: "pre",
     transform(code: string, id: string) {
-      if (id.endsWith(".ts") || id.endsWith(".tsx")) {
+      if (
+        id.endsWith(".ts") ||
+        id.endsWith(".tsx") ||
+        id.endsWith(".js") ||
+        id.endsWith(".jsx")
+      ) {
         return {
           code: transformEasyThreadFunctions(code),
           map: null,
